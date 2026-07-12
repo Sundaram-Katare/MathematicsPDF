7f5cd731aa2cabbfac00033f94b3afd5e443cdd3
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-07-12 19:33:54 +0530

    feat: initialized the express server and setup folder structure

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
